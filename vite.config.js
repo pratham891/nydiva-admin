63727e4b6f18613ca0497379988aab734efb789a
Pratham Singh <prathambsingh891@gmail.com>
2025-01-21 15:31:10 +0530

    Add dotenv dependency and configure Vite to use environment variables for backend API URL

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+import dotenv from 'dotenv'
+dotenv.config()
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: 'https://nydiva-backend.vercel.app',
+        target: process.env.VITE_NYDIVA_BACKEND,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
